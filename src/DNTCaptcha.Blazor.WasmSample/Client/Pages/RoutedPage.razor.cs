f8e2231ae9922b97686c2eaec3429a0f23f2ace1
VahidN <vahid_nasiri@yahoo.com>
2022-11-15 13:06:24 +0330

    Upgrade to .NET 7.0

---
+++ src/DNTCaptcha.Blazor.WasmSample/Client/Pages/RoutedPage.razor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DNTCaptcha.Blazor.WasmSample.Shared.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace DNTCaptcha.Blazor.WasmSample.Client.Pages;
+
+public partial class RoutedPage : ComponentBase
+{
+    private LoginViewModel Model { set; get; } = new();
+
+    private async Task DoLoginAsync()
+    {
+        await Task.Delay(500);
+        Console.WriteLine(JsonSerializer.Serialize(Model));
+
+        // Clear the form, Redraw the captcha
+        Model = new LoginViewModel();
+        Model.CaptchaText1 = string.Empty; // How to redraw the captcha
+    }
+}
